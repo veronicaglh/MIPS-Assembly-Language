cee997394a80aedb7c3594ea9634ec794944bd54
veronicaglh <veronicagt.IT@gmail.com>
2022-09-19 23:55:16 +0300

    add program to add float

---
+++ Assembly Basics/addFloat.asm
@@ -0,0 +1,19 @@
+#This file will be used to add two floats 
+.data 
+floatNum1: .float 3.12
+floatNum2: .float 3.14 
+message: .asciiz "The sum of the two float numbers is: "
+
+# Declaring and intializing variables
+.text 
+lwc1 $f1, floatNum1
+lwc1 $f2, floatNum2
+add.s $f3,$f2,$f1
+
+li $v0, 4
+la $a0, message
+syscall 
+
+li $v0, 2
+mov.s $f12, $f3
+syscall 
